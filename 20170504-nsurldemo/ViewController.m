95f5a9c9e3cbb6180510a6c0f49c2820746fd2cc
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2017-05-04 19:56:38 +0300

    try to get a webpage from gmail

--- 20170504-nsurldemo/ViewController.m
+++ 20170504-nsurldemo/ViewController.m
@@ -16,7 +16,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSURLSessionConfiguration *defaultConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+//    NSString *cacheDirectory = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+//    NSString *cachePath = [cacheDirectory stringByAppendingPathComponent:@"MyCache"];
+//    NSURLCache
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:defaultConfiguration];
+    NSURL *url = [NSURL URLWithString:@"https://www.gmail.com"];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"got response %@ with error %@.\n", response, error);
+        NSString *responseStr = [[NSString alloc] initWithData:data encoding:NSWindowsCP1251StringEncoding];
+        NSLog(@"data: %@", responseStr);
+    }];
+    [task resume];
 }
 
 
