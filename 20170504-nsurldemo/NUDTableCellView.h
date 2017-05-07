b9a652139940944420ba6e29d1db0101e996e0f4
Dmitry A. Zvorykin <dmitry.zvorikin@gmail.com>
2017-05-07 17:07:09 +0300

    added cell

---
+++ 20170504-nsurldemo/NUDTableCellView.h
@@ -0,0 +1,13 @@
+//
+//  NUDTableCellView.h
+//  20170504-nsurldemo
+//
+//  Created by Admin on 07/05/2017.
+//  Copyright © 2017 iOS-School-1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NUDTableCellView : NSObject
+
+@end
---
+++ 20170504-nsurldemo/NUDTableCellView.m
@@ -0,0 +1,13 @@
+//
+//  NUDTableCellView.m
+//  20170504-nsurldemo
+//
+//  Created by Admin on 07/05/2017.
+//  Copyright © 2017 iOS-School-1. All rights reserved.
+//
+
+#import "NUDTableCellView.h"
+
+@implementation NUDTableCellView
+
+@end
